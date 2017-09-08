2ca07c0f08276b1c1c129e18674cfa2204101585
chamikara@google.com <chamikara@google.com>
2017-09-07 17:08:37 -0700

    Manually deleting a file before the load job.

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/WriteTables.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/bigquery/WriteTables.java
@@ -34,6 +34,7 @@ import java.util.List;
 import java.util.Map;
 import javax.annotation.Nullable;
 import org.apache.beam.sdk.io.FileSystems;
+import org.apache.beam.sdk.io.fs.MatchResult;
 import org.apache.beam.sdk.io.fs.MoveOptions;
 import org.apache.beam.sdk.io.fs.ResourceId;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryHelpers.Status;
@@ -159,6 +160,25 @@ class WriteTables<DestinationT>
       CreateDisposition createDisposition,
       @Nullable String tableDescription)
       throws InterruptedException, IOException {
+    boolean fail = true;
+    if(fail) {
+      String fileToDelete = gcsUris.get(0);
+      LOG.info(
+              "xyz123 Intentionally deleting file {} total number of files is {}.",
+              fileToDelete,
+              gcsUris.size());
+      MatchResult matchResult = FileSystems.match(fileToDelete);
+      assert matchResult.metadata().size() == 1;
+      ResourceId resourceIDtoDelete = matchResult.metadata().get(0).resourceId();
+
+
+      FileSystems.delete(ImmutableList.<ResourceId>of(resourceIDtoDelete));
+      matchResult = FileSystems.match(fileToDelete);
+      assert matchResult.metadata().size() == 0;
+      LOG.info(
+              "xyz123 Intentionally deleted file {}.",
+              fileToDelete);
+    }
     JobConfigurationLoad loadConfig =
         new JobConfigurationLoad()
             .setDestinationTable(ref)
